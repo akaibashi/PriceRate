235d4073c3affe7cee046cb006006808d04aecbb
赤井橋 健2 <akaihashi2@akaibashi-no-MacBook-Pro.local>
2013-07-06 16:34:40 +0900

    Initial Commit

---
+++ price_rate/PriceRateAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PriceRateAppDelegate.h
+//  price_rate
+//
+//  Created by 赤井橋 健2 on 2013/07/06.
+//  Copyright (c) 2013年 ken akaibashi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PriceRateAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ price_rate/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  price_rate
+//
+//  Created by 赤井橋 健2 on 2013/07/06.
+//  Copyright (c) 2013年 ken akaibashi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PriceRateAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PriceRateAppDelegate class]));
+    }
+}
